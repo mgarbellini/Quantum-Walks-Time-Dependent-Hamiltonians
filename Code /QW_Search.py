cbda257ab90437adbd680dc5580f1b1ab54653c7
matteogarbellini <matteo.garbellini@studenti.unimi.it>
2021-11-22 18:32:24 +0100

    Rewrite based on #3 and #4

---
+++ Code /QW_Search.py
@@ -0,0 +1,27 @@
+"""
+M. Garbellini
+Dept. of Physics
+Universita degli Studi di Milano
+matteo.garbellini@studenti.unimi.it
+
+Quantum Walks with time-dependent Hamiltonians:
+application to the search problem on graphs
+
+This code is a rewritten version of the code used for my bachelor thesis
+at the University of Milan. The old version can be found at my github page
+github.com/mgarbellini.
+
+Major features and enhancement compare to
+the previous version.
+
+- QW Spatial search on graph with time-independent Hamiltonian (see Fahri & Gutmann)
+- QW Spatial search with time-dependent Hamiltonian
+- Different graph topology implemented: circle, complete, random, star, etc..
+
+
+- Robustness of the approach
+- Localization and Multiple iteration search
+
+
+
+"""
